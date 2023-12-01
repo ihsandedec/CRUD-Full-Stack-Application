963bea1e71ba9cb094c73fe234bb8c9daf8ce8ec
ddcihsan <ddcihsan@gmail.com>
2023-12-01 23:18:46 +0300

    I added a header for the page design

---
+++ react-intro/HeaderComponent.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+export default class HeaderComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            
+        }
+    }
+  render() {
+    return (
+      <div>
+        <header>
+            <nav className='navbar navbar-expand-md navbar-dark bg-dark'>
+            <div><a href="https://javaquides.net"  className='navbar-brand'>
+              Employee Management App
+              </a></div>
+            </nav>
+        </header>
+       </div>
+    )
+  }
+}
+
+
